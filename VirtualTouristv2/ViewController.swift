7b7cea05b53804cc1131eb71573bd3b25821963a
Sean Goldsborough <smgoldsborough@hotmail.com>
2018-04-02 16:25:08 -0400

    Initial Commit

---
+++ VirtualTouristv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTouristv2
+//
+//  Created by Sean Goldsborough on 4/2/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
